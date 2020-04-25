a982665e499693f7e6f9514d4542c6d613adb4bc
LBJames2332 <1846136263@qq.com>
2020-04-25 19:22:28 +0800

    经典最长公共子序列，进阶版1维DP，实现，数组是真的要好好想清楚了再下手啊

--- src/Finished/LongestSubSequence.java
+++ src/Finished/LongestSubSequence.java
@@ -3,7 +3,7 @@ package Finished;
 public class LongestSubSequence {
     //经典问题最长子序列
     public static void main(String[] args) {
-        System.out.println(longestCommonSubsequence("bsbininm","jmjkbkjkv" ));
+        System.out.println(longestCommonSubsequence_better("bsbininm","jmjkbkjkv"));
     }
 
     /**
@@ -13,6 +13,7 @@ public class LongestSubSequence {
      * @return
      */
     public static int longestCommonSubsequence(String text1, String text2) {
+        System.out.println("break");
         int[][] record = new int[text1.length()+1][text2.length()+1];
         char[] c1 = text1.toCharArray();
         char[] c2 = text2.toCharArray();
@@ -47,6 +48,38 @@ public class LongestSubSequence {
             }
         }
         return record[record.length-1][record[0].length-1];
+
+    }
+
+    /**
+     * DP进阶版
+     * 执行用时 :8 ms, 在所有 Java 提交中击败了93.22%的用户
+     * 内存消耗 :37.8 MB, 在所有 Java 提交中击败了8.33%的用户
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public static int longestCommonSubsequence_better(String text1, String text2) {
+        int top;
+        int left_top = 0;
+        int[] left = new int[text1.length()];
+        char[] c1 = text1.toCharArray();
+        char[] c2 = text2.toCharArray();
+        int nextleft_top;
+        for (int i = 0; i < text2.length(); i++) {
+            //开始一列的求解
+            top = 0;
+            left_top = 0;
+            for (int j = 0; j < text1.length(); j++) {
+                //一列一列的求
+                nextleft_top = left[j];
+                if (c1[j]==c2[i]) left[j] = 1+left_top;
+                else left[j] = ((left[j]>top)?left[j]:top);
+                top = left[j];
+                left_top = nextleft_top;
+            }
+        }
+        return left[left.length-1];
 
     }
 }
