1eca8606290c8df9d74b9debfb8bb881c7046e4b
LBJames2332 <1846136263@qq.com>
2020-04-30 21:50:37 +0800

    计算数组A中所有子序列的宽度之和，数学推导，公式法实现

--- src/Finished/SumOfTheWidthOfAllSubSequences.java
+++ src/Finished/SumOfTheWidthOfAllSubSequences.java
@@ -9,6 +9,8 @@ public class SumOfTheWidthOfAllSubSequences {
      * 返回 A 的所有子序列的宽度之和。
      * 由于答案可能非常大，请返回答案模 10^9+7。
      * 链接：https://leetcode-cn.com/problems/sum-of-subsequence-widths
+     * 执行用时 :19 ms, 在所有 Java 提交中击败了98.55%的用户
+     * 内存消耗 :42.7 MB, 在所有 Java 提交中击败了100.00%的用户
      * @param A
      * @return
      */
@@ -18,15 +20,24 @@ public class SumOfTheWidthOfAllSubSequences {
         pow2[0] = 1;
         for (int i = 1; i < A.length; ++i)
             pow2[i] = pow2[i-1] * 2 % MOD;
+//        Arrays.sort(A);
+//        long Sum = 0;
+//        for (int i = 0; i < A.length; i++) {
+//            for (int j = i+1; j < A.length; j++) {
+//                Sum= (long) ((Sum+(A[j]-A[i])*pow2[j-i-1])%1_000_000_007);
+//            }
+//        }
+//        return (int) Sum;
+
+        int N = A.length;
         Arrays.sort(A);
-        long Sum = 0;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = i+1; j < A.length; j++) {
-                Sum= (long) ((Sum+(A[j]-A[i])*pow2[j-i-1])%1_000_000_007);
-            }
-        }
-        return (int) Sum;
 
 
+
+        long ans = 0;
+        for (int i = 0; i < N; ++i)
+            ans = (ans + (pow2[i] - pow2[N-1-i]) * A[i]) % MOD;
+
+        return (int) ans;
     }
 }
