465e37745014964b8694cfc0562378f9061d2771
LBJames2332 <1846136263@qq.com>
2020-05-03 07:31:53 +0800

    镜像二叉树，不难，实现。

---
+++ src/Finished/MirrorBinaryTree.java
@@ -0,0 +1,65 @@
+package Finished;
+
+import java.util.Stack;
+
+public class MirrorBinaryTree {
+    /**
+     * 执行用时 :1 ms, 在所有 Java 提交中击败了33.97%的用户
+     * 内存消耗 :37.8 MB, 在所有 Java 提交中击败了100.00%的用户
+     */
+    Stack<TreeNode2> stack_tree = new Stack<>();
+    public boolean isSymmetric(TreeNode2 root) {
+        if (root.right!=null&&root.left!=null){
+            if (root.right.val==root.left.val){
+                stack_tree.add(root.left);
+                stack_tree.add(root.right);
+                return Loop_Judge();
+            }
+        }
+        else {
+            if (!(root.right==null&&root.left==null)){
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean Loop_Judge() {
+        TreeNode2 node1;
+        TreeNode2 node2;
+        while (!stack_tree.empty()){
+            node1 = stack_tree.pop();
+            node2 = stack_tree.pop();
+            if (node1.left!=null&&node2.right!=null){
+                if (node1.left.val==node2.right.val){
+                    stack_tree.add(node1.left);
+                    stack_tree.add(node2.right);
+                }
+                else return false;
+            }
+            else {
+                if (!(node1.left==null&&node2.right==null)){
+                    return false;
+                }
+            }
+            if (node1.right!=null&&node2.left!=null){
+                if (node1.right.val==node2.left.val){
+                    stack_tree.add(node1.right);
+                    stack_tree.add(node2.left);
+                }
+                else return false;
+            }
+            else {
+                if (!(node1.right==null&&node2.left==null)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+class TreeNode2 {
+    int val;
+    TreeNode2 left;
+    TreeNode2 right;
+    TreeNode2(int x) { val = x; }
+}
