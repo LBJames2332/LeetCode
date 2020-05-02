dc4ba2cc3c9360d3f04d5e2a804fbb57dc23d92b
LBJames2332 <1846136263@qq.com>
2020-05-03 06:05:17 +0800

    最长定差子序列，暴力法，map法，实现，多思考再下手

--- src/Finished/LongestArithmeticSequenceInDiff.java
+++ src/Finished/LongestArithmeticSequenceInDiff.java
@@ -1,3 +1,8 @@
+package Finished;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestArithmeticSequenceInDiff {
     /**
      * 给你一个整数数组 arr 和一个整数 difference，请你找出 arr 中所有相邻元素之间的差等于给定 difference 的等差子序列，并返回其中最长的等差子序列的长度。
@@ -30,5 +35,25 @@ public class LongestArithmeticSequenceInDiff {
     public static void main(String[] args) {
         LongestArithmeticSequenceInDiff longestArithmeticSequenceInDiff = new LongestArithmeticSequenceInDiff();
         System.out.println(longestArithmeticSequenceInDiff.longestSubsequence(new int[]{1,5,7,8,5,3,4,2,1}, -2));
+    }
+
+    /**
+     * 执行用时 :63 ms, 在所有 Java 提交中击败了35.69%的用户
+     * 内存消耗 :59.2 MB, 在所有 Java 提交中击败了25.00%的用户
+     * @param arr
+     * @param difference
+     * @return
+     */
+    public int longestSubsequence_map(int[] arr, int difference) {
+        int length = 1;
+        Map<Integer,Integer> dp = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!dp.containsKey(arr[i] - difference)){
+                dp.put(arr[i],dp.get(arr[i] - difference)+1);
+                length = (length> dp.get(arr[i]))?length: dp.get(arr[i]);
+            }
+            else dp.put(arr[i],1);
+        }
+        return length;
     }
 }
