e402c1775b20ff8e7851a31637a9704351d85bc9
LBJames2332 <1846136263@qq.com>
2020-05-16 18:54:57 +0800

    补充 路径和问题Ⅰ和Ⅱ的题目描述

--- src/Finished/SumOfRoad.java
+++ src/Finished/SumOfRoad.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class SumOfRoad {
     /**
+     * 给定一个二叉树和一个目标和，判断该树中是否存在“根节点”到“叶子节点”的路径，这条路径上所有节点值相加等于目标和。
      * 执行用时 :0 ms, 在所有 Java 提交中击败了100.00%的用户
      * 内存消耗 :39.5 MB, 在所有 Java 提交中击败了6.52%的用户
      * @param root
@@ -33,6 +34,7 @@ public class SumOfRoad {
     ArrayList<Integer> ll = new ArrayList<>();
 
     /**
+     * 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的“路径”。
      * 执行用时 :1 ms, 在所有 Java 提交中击败了99.98%的用户
      * 内存消耗 :39.9 MB, 在所有 Java 提交中击败了5.26%的用户
      * @param root
