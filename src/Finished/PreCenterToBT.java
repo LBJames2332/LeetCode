2599931616bd42838450dd1e179444f5f0f45998
LBJames2332 <1846136263@qq.com>
2020-05-22 07:52:36 +0800

    优化一下栈空间

---
+++ src/Finished/PreCenterToBT.java
@@ -0,0 +1,35 @@
+package Finished;
+
+/**
+ * 执行用时 :4 ms, 在所有 Java 提交中击败了63.79%的用户
+ * 内存消耗 :39.8 MB, 在所有 Java 提交中击败了73.33%的用户
+ */
+public class PreCenterToBT {
+    int[] preorder;
+    int[] inorder;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+        return getRoot(0,preorder.length-1,0,preorder.length-1);
+    }
+    public TreeNode getRoot(int start_p,int end_p,int start_c,int end_c){
+        if(start_p > end_p) return null;
+        else {
+            TreeNode root = new TreeNode(preorder[start_p]);
+            for (int i = start_c; i <= end_c; i++) {
+                if (inorder[i]==root.val){
+                    root.left = getRoot(start_p+1,start_p+i-start_c,start_c,i-1);
+                    root.right = getRoot(start_p+i-start_c+1,end_p,i+1,end_c);
+                    break;
+                }
+            }
+            return root;
+        }
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
