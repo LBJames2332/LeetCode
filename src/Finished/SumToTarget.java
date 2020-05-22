5dee375dff57848c6a2d49714276dfaa945d3172
LBJames2332 <1846136263@qq.com>
2020-05-22 15:30:07 +0800

    空间复杂度优化为O(N)，测试结果却差别不大，搞不懂。。。 实现。

--- src/Finished/SumToTarget.java
+++ src/Finished/SumToTarget.java
@@ -1,9 +1,9 @@
 package Finished;
 
 public class SumToTarget {
-    int[][] dp;
+    /*int[][] dp;
     int length;
-    int width;
+    int width;*/
 
     /**
      * 执行用时 :14 ms, 在所有 Java 提交中击败了62.01%的用户
@@ -12,7 +12,7 @@ public class SumToTarget {
      * @param S
      * @return
      */
-    public int findTargetSumWays(int[] nums, int S) {
+    /*public int findTargetSumWays(int[] nums, int S) {
         length = nums.length;
         int sum = 0;
         int help_x;
@@ -37,8 +37,48 @@ public class SumToTarget {
         }
         return dp[length][S+sum];
 
-    }
+    }*/
+    int[][] dp;
+    int length;
+    int width;
 
+    /**
+     * 执行用时 :10 ms, 在所有 Java 提交中击败了73.31%的用户
+     * 内存消耗 :39.5 MB, 在所有 Java 提交中击败了6.67%的用户
+     * @param nums
+     * @param S
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int S) {
+        length = nums.length;
+        int sum = 0;
+        int help_x;
+        for (int i = 0; i < length; i++) {
+            sum+=nums[i];
+        }
+        if (S > sum) return 0;
+        width = sum<<1;
+        dp = new int[2][width+1];
+        dp[0][sum] = 1;
+        int cur_index = 1;
+        int prev_index = 0;
+        for (int i = 1; i < length; i++) {
+            help_x = i-1;
+
+            for (int j = 0; j < sum; j++) {
+                dp[cur_index][j] = getPos(prev_index,j-nums[help_x])+getPos(prev_index,j+nums[help_x]);
+                dp[cur_index][width-j] = dp[cur_index][j];
+            }
+            dp[cur_index][sum] = getPos(prev_index,sum-nums[help_x])+getPos(prev_index,sum+nums[help_x]);
+            prev_index = cur_index;
+            cur_index ^= 1;
+        }
+        help_x = length-1;
+        for (int i = 0; i <=S+sum ; i++) {
+            dp[cur_index][i] = getPos(prev_index,i-nums[help_x])+getPos(prev_index,i+nums[help_x]);
+        }
+        return dp[cur_index][S+sum];
+    }
     public static void main(String[] args) {
         SumToTarget sumToTarget = new SumToTarget();
         System.out.println(sumToTarget.findTargetSumWays(new int[]{1,1,1,1,1},3));
