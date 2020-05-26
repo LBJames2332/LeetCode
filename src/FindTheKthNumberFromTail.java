b9588364d0e31d6efd2ef8031f42ef0df828a239
LBJames2332 <1846136263@qq.com>
2020-05-26 19:51:06 +0800

    这个太简单了，无需多言。

---
+++ src/FindTheKthNumberFromTail.java
@@ -0,0 +1,29 @@
+public class FindTheKthNumberFromTail {
+    /**
+     * 执行用时 :0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗 :37.4 MB, 在所有 Java 提交中击败了100.00%的用户
+     * @param head
+     * @param k
+     * @return
+     */
+    public int kthToLast(ListNode head, int k) {
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow.val;
+    }
+    class ListNode{
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
