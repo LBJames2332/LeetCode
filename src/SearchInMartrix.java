8d50ba27fc5ffddca5586a1106afa0b5678d2d9c
LBJames2332 <1846136263@qq.com>
2020-05-28 19:01:01 +0800

    二维矩阵查找目标值是否存在。 思路如下： 以左下（或右上）的点为起点。 因为可以确定小于它的数都在它的上面，大于它的数都在它的右边; 所以根据大小关系调整当前位置:        若当前值过大，则上移1位，排除掉一排比它大的数;        反之同理。 实现。

---
+++ src/SearchInMartrix.java
@@ -0,0 +1,23 @@
+public class SearchInMartrix {
+    /**
+     * 执行用时 :6 ms, 在所有 Java 提交中击败了99.80%的用户
+     * 内存消耗 :45.3 MB, 在所有 Java 提交中击败了17.86%的用户
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int x = matrix.length-1;
+        int y = 0;
+        int help;
+        while (x>-1&&x<matrix.length&&y>-1&&y<matrix[0].length){
+            help = matrix[x][y] - target;
+            if (help==0)return true;
+            else if (help > 0){
+                x--;
+            }
+            else y++;
+        }
+        return false;
+    }
+}
