86c9ffce75f254c27bc2e50c2d18a3de3fa2ef06
LBJames2332 <1846136263@qq.com>
2020-05-28 09:08:47 +0800

    字符串解码问题。 给定一个经过编码的字符串，返回它解码后的字符串。
    编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
    
    你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
    
    此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
    
    示例:
    
    s = "3[a]2[bc]", 返回 "aaabcbc".
    s = "3[a2[c]]", 返回 "accaccacc".
    s = "2[abc]3[cd]ef", 返回 "abcabccdcdcdef".
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/decode-string
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
    
    用两个栈实现的方法，思路如下：
    
    1.当读到数字，我们就找到这个数字的起始位置，计算出数值并压入存储出现次数的栈中。
    
    2.当读到‘]’意味着应该完成一组[]的祖父穿复制工作。
          读出栈顶的数字，并将前一个‘[’之后的数字找出来，再按照个数压入栈中。
    
    3.字符串遍历完毕，将栈中的字符转为字符串输出。
    
    实现，可能有更好的方法，之后再研究研究。

---
+++ src/deCodeStr.java
@@ -0,0 +1,57 @@
+import java.util.Stack;
+
+public class deCodeStr {
+    /**
+     * 执行用时 :3 ms, 在所有 Java 提交中击败了38.53%的用户
+     * 内存消耗 :38.4 MB, 在所有 Java 提交中击败了7.69%的用户
+     * @param s
+     * @return
+     */
+    public String decodeString(String s) {
+        char[] char_s = s.toCharArray();
+        StringBuilder help_sb = new StringBuilder();
+        Stack<Integer> count = new Stack<>();
+        Stack<Character> str = new Stack<>();
+        int times;
+        int i = 0;
+        int begin;
+        while (true){
+            if (i==char_s.length) break;
+            if ('0'<=char_s[i]&&char_s[i]<='9'){
+                //从现在开始看看给的数字是多少
+                begin = i++;
+                while ('0'<=char_s[i]&&char_s[i]<='9') i++;
+                times = Integer.valueOf(s.substring(begin,i)).intValue();
+                count.push(times);
+            }
+            if (char_s[i]==']'){
+                //该出栈了
+                help_sb.delete( 0, help_sb.length());//从0开始，删这么长的内容
+                while (str.peek()!='[') help_sb.insert(0,str.pop());
+                str.pop();
+                int c = count.pop();
+                for (int j = 0; j < c; j++) {
+                    for (int k = 0; k < help_sb.length(); k++) {
+                        str.push(help_sb.charAt(k));
+                    }
+                }
+                i++;
+
+            }
+            else str.push(char_s[i++]);
+
+        }
+        return toString(str);
+    }
+
+    public String toString(Stack<Character> stack) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!stack.isEmpty()) stringBuilder.insert(0,stack.pop());
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        deCodeStr deCodeStr = new deCodeStr();
+        System.out.println(deCodeStr.decodeString("2[abc]3[cd]ef"));
+    }
+}
