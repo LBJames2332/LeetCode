a78755ab0fe543787b259c371dd98a0788dd9de3
LBJames2332 <1846136263@qq.com>
2020-05-29 11:18:23 +0800

    这题就是左神说的那个题，没什么好说的，差题！

---
+++ src/Finished/DeleteNode.java
@@ -0,0 +1,17 @@
+package Finished;
+
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+    class ListNode{
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
