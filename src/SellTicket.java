1666483dbac72b1e0d8275b028851504719c3c10
LBJames2332 <1846136263@qq.com>
2020-06-01 17:33:24 +0800

    买卖股票，没什么好说的，实在太简单了。

---
+++ src/SellTicket.java
@@ -0,0 +1,18 @@
+public class SellTicket {
+    /**
+     * 执行用时 :1 ms, 在所有 Java 提交中击败了98.81%的用户
+     * 内存消耗 :39.6 MB, 在所有 Java 提交中击败了5.32%的用户
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        if (prices.length==0) return 0;
+        int max = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+             max = Math.max(max,prices[i]-min);
+             min = (min < prices[i])?min:prices[i];
+        }
+        return max;
+    }
+}
