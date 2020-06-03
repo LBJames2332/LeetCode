a8d72089680829f8d5331cb4f38fe3982a1249e1
LBJames2332 <1846136263@qq.com>
2020-06-03 14:18:12 +0800

    最短路径和，动态规划二维数组，实现。 可以改成一维数组的，这里就不展开了。

--- src/Finished/MinimumPathWeight.java
+++ src/Finished/MinimumPathWeight.java
@@ -31,7 +31,28 @@ public class MinimumPathWeight {
         process(c_r+1,c_c,weight);
         process(c_r,c_c+1,weight);
     }
-
+    /**
+     * 执行用时 :3 ms, 在所有 Java 提交中击败了89.89%的用户
+     * 内存消耗 :42.7 MB, 在所有 Java 提交中击败了24.24%的用户
+     */
+    int[][]memo;
+    public int minPathSum_memo(int[][] grid) {
+        GRID = grid;
+        r_max = grid.length;
+        c_max = grid[0].length;
+        memo = new int[r_max][c_max];
+        memo[0][0] = grid[0][0];
+        for (int i = 1; i < c_max; i++) {
+            memo[0][i] = memo[0][i-1]+grid[0][i];
+        }
+        for (int i = 1; i < r_max; i++) {
+            memo[i][0] = memo[i-1][0] + grid[i][0];
+            for (int j = 1; j < c_max; j++) {
+                memo[i][j] = ((memo[i-1][j]>memo[i][j-1])?memo[i][j-1]:memo[i-1][j])+grid[i][j];
+            }
+        }
+        return memo[r_max-1][c_max-1];
+    }
 
     public static void main(String[] args) {
         MinimumPathWeight minimumPathWeight = new MinimumPathWeight();
