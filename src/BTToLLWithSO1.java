b4b4d1d19c9b126d8ba85669d87657773868fc89
LBJames2332 <1846136263@qq.com>
2020-06-04 16:54:27 +0800

    在原二叉树的基础上将二叉树按先序遍历转为链表（树节点） 用迭代的方法实现，SO(1) 1.若有左子树:      a.左子树的根接到根的右节点      b.根的原右节点接到该子树的最右下角节点的右节点 root=root.right; !!!break case:root==null 实现。

--- src/BTToLLWithSO1.java
+++ src/BTToLLWithSO1.java
@@ -27,5 +27,30 @@ public class BTToLLWithSO1 {
         else Arrange(curr.right);
         return curr;
     }
+    /**
+     * 执行用时 :0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗 :38.9 MB, 在所有 Java 提交中击败了6.67%的用户
+     * @param root
+     */
+    private void flatten_O1(TreeNode root) {
+        if (root==null) return;
+        TreeNode rightest;
+        while(root!=null) {
 
+            if (root.left != null){
+                //找其左子树的最有节点
+                rightest = root.left;
+                while (rightest.right != null) {
+                    rightest = rightest.right;
+                }
+                rightest.right = root.right;
+                root.right = root.left;
+                root.left = null;
+
+            }
+            root = root.right;
+        }
+    }
 }
+
+
