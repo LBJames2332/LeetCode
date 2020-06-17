1e27115a2df49aa88eb2bf8a8cda0c74c1727898
LBJames2332 <1846136263@qq.com>
2020-06-17 20:21:51 +0800

    找到终点站 给你一份旅游线路图，该线路图中的旅行线路用数组 paths 表示，其中 paths[i] = [cityAi, cityBi] 表示该线路将会从 cityAi 直接前往 cityBi 。请你找出这次旅行的终点站，即没有任何可以通往其他城市的线路的城市。
    题目数据保证线路图会形成一条不存在循环的线路，因此只会有一个旅行终点站。
    
    因为不存在环又只有1个终点，所以必然是一条链路。
    言尽于此。
    实现。

---
+++ src/FindEnd.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindEnd {
+    /**
+     * 执行用时 :5 ms, 在所有 Java 提交中击败了23.72%的用户
+     * 内存消耗 :39.3 MB, 在所有 Java 提交中击败了100.00%的用户
+     * @param paths
+     * @return
+     */
+    public String destCity(List<List<String>> paths) {
+        Map<String,Integer> ans = new HashMap<>();
+        for (List<String> ll:paths){
+            ans.put(ll.get(0),ans.getOrDefault(ll.get(0),0)+2);
+            ans.put(ll.get(1),ans.getOrDefault(ll.get(1),0)+1);
+        }
+        for (Map.Entry<String,Integer>entry:ans.entrySet()){
+            if (entry.getValue()==1)return entry.getKey();
+        }
+        return null;
+    }
+}
